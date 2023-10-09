3a5910ad78c227413c5f48f9774cd7b34e3a8b37
531428aj <531428@escg.ac.uk>
2023-10-09 15:55:16 +0100

    test

--- Assets/Enemy follow.cs
+++ Assets/Enemy follow.cs
@@ -1,15 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class Enemyfollow : MonoBehaviour {
     public GameObject player;
 
+    public bool flip;
+
+    public float speed;
+
 
     private void Update() {
         Vector3 scale = transform.localScale;
 
-        if (player.transform.position.x > transform)
+        if (player.transform.position.x > transform.position.x)
+        {
+            scale.x = Mathf.Abs(scale.x) * -1 * (flip ? -1 : 1);
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            scale.x = Mathf.Abs(scale.x) (flip ? -1 : 1);
+            transform.Translate(speed * Time.deltaTime * -1, 0, 0);
+        }
 
             transform.localScale = scale;
 
